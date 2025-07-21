ae6ddbbdeb2d2e34f2c271c562b066abe249b2b9
Parames Pasitprachaja <paramespasitprachaja@Paramess-MacBook-Air.local>
2025-07-22 03:15:27 +0700

    feat(db): get event by id from database

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"net/http"
+	"strconv"
 
 	"github.com/PrinceM13/go-event-booking/db"
 	"github.com/PrinceM13/go-event-booking/models"
@@ -17,6 +18,7 @@ func main() {
 	})
 
 	server.GET("/events", getEvents)
+	server.GET("/events/:id", getEvent)
 	server.POST("/events", createEvent)
 
 	server.Run(":8080")
@@ -31,6 +33,26 @@ func getEvents(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{"events": events})
 }
 
+func getEvent(c *gin.Context) {
+	eventId, err := strconv.ParseInt(c.Param("id"), 10, 64)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid event ID"})
+		return
+	}
+
+	event, err := models.GetEventByID(eventId)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"message": "Failed to fetch event, please try again later."})
+		return
+	}
+	if event == nil {
+		c.JSON(http.StatusNotFound, gin.H{"message": "Event not found"})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"event": event})
+}
+
 func createEvent(c *gin.Context) {
 	var event models.Event
 	if err := c.ShouldBindJSON(&event); err != nil {
@@ -39,7 +61,7 @@ func createEvent(c *gin.Context) {
 	}
 
 	// Mock saving the event
-	event.UserID = 1
+	event.UserID = 13
 
 	if err := event.Save(); err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"message": "Failed to create event, please try again later."})
