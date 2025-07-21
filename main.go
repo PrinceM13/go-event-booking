b276b51b5b81a55adccbc4e68104a27cd7df98bf
Parames Pasitprachaja <paramespasitprachaja@Paramess-MacBook-Air.local>
2025-07-21 11:56:40 +0700

    feat(db): initial db with sqlite

--- main.go
+++ main.go
@@ -3,11 +3,13 @@ package main
 import (
 	"net/http"
 
+	"github.com/PrinceM13/go-event-booking/db"
 	"github.com/PrinceM13/go-event-booking/models"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	db.InitDB()
 	server := gin.Default()
 
 	server.GET("/health", func(c *gin.Context) {
